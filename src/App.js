a3d1e5ba33ea1362566ac4ba850ee8eaac6419c7
Mateusz Myszke <myszke93@gmail.com>
2023-03-20 14:39:56 +0100

    Add navBar, footer, header

--- src/App.js
+++ src/App.js
@@ -5,20 +5,25 @@ import PostAdd from './components/pages/PostAdd/PostAdd';
 import PostEdit from './components/pages/PostEdit/PostEdit';
 import About from './components/pages/About/About';
 import NotFound from './components/pages/NotFound/NotFound';
-
+import Footer from './components/views/Footer/Footer';
+import { Container } from 'react-bootstrap';
+import Header from './components/views/Header/Header';
 function App() {
   return (
-    <div className="App">
-      <p>Hello world</p>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/post/:id' element={<Post />} />
-        <Route path='/post/add' element={<PostAdd />} />
-        <Route path='/post/edit/:id' element={<PostEdit />} />
-        <Route path='/about' element={<About />} />
-        <Route path='*' element={<NotFound />} />
-      </Routes>
-    </div>
+    <main>
+      <Container> 
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/post/:id' element={<Post />} />
+          <Route path='/post/add' element={<PostAdd />} />
+          <Route path='/post/edit/:id' element={<PostEdit />} />
+          <Route path='/about' element={<About />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </Container>
+    </main>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,16 +4,17 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter> 
-    <Provider store={store}>  
-      <React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter> 
+      <Provider store={store}>  
         <App />
-      </React.StrictMode>
-    </Provider>
-  </BrowserRouter>
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
 
