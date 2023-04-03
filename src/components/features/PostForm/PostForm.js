2ff702d37eede19c0b7f873a293dfe0df10646ab
Mateusz Myszke <myszke93@gmail.com>
2023-04-03 10:43:45 +0200

    Add validate dont works correctly titlt, author, shortDescription

--- src/components/features/PostForm/PostForm.js
+++ src/components/features/PostForm/PostForm.js
@@ -14,12 +14,18 @@ const PostForm = ({ action, actionText, ...props }) => {
   const [publishedDate, setPublishedDate] = useState(props.publishedDate || '');
   const [shortDescription, setShortDescription] = useState(props.shortDescription || '');
   const [content, setContent] = useState(props.content || '');
+  const [contentError, setContentError] = useState(false);
+  const [dateError, setDateError] = useState(false);
   
   const { register, handleSubmit: validate, formState: { errors } } = useForm();
 
-  const handleSubmit = e => {
-    action({ title, shortDescription, content, publishedDate, author});
-  }
+  const handleSubmit = (e) => {
+    setContentError(!content)
+    setDateError(!publishedDate)
+    if(content && publishedDate) {
+      action({ title, author, publishedDate, shortDescription, content });
+    }
+  };
 
   return(
     <Form onSubmit={validate(handleSubmit)}> 
@@ -27,27 +33,64 @@ const PostForm = ({ action, actionText, ...props }) => {
      <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Title</Form.Label>
         <Form.Control
-          {...register("title", { required: true })}
+          {...register('title', { required: true, minLength: 3 })}
           value={title}
           onChange={e => setTitle(e.target.value)}
-          type="text" placeholder="Enter title"
+          type="text"
+          placeholder="Enter title"
         />
-        {errors.title && <span>This field is required</span>}  
+        {errors.title && 
+          <small className='d-block form-text text-danger mt-2'>
+            This field is required (min 3 characters)
+          </small>
+        }
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
         <Form.Label>Author </Form.Label>
-        <Form.Control  placeholder='Enter author' value={author} onChange={e => setAuthor(e.target.value)} />
+        <Form.Control 
+          {...register('author', { required: true, minLength: 3 })} 
+          placeholder='Enter author' 
+          value={author} 
+          onChange={e => setAuthor(e.target.value)} 
+        />
+        {errors.author && <small className="d-block form-text text-danger mt-2">
+          Author is too short (min is 3)</small>}
       </Form.Group> 
 
-      <DatePicker selected={publishedDate} onChange={(date) => setPublishedDate(date)} />
+      <Form.Group className='mb-3 w-50'>
+        <Form.Label>Published</Form.Label>
+        <DatePicker 
+          selected={publishedDate} 
+          onChange={(date) => setPublishedDate(date)} 
+        />
+        {dateError && <small className="d-block form-text text-danger mt-2">
+          Date can't be empty</small>}
+      </Form.Group>
       
       <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
         <Form.Label>Short description </Form.Label>
-        <Form.Control as="textarea" rows={3} placeholder='Leave a comment here' value={shortDescription} onChange={e => setShortDescription(e.target.value)} />
+        <Form.Control 
+          {...register('shortDescription', { required: true, minLength: 20 })}
+          as="textarea" 
+          rows={3} 
+          placeholder='Leave a comment here' 
+          value={shortDescription} 
+          onChange={e => setShortDescription(e.target.value)} 
+        />
+        {errors.shortDescription && <small className="d-block form-text text-danger mt-2">
+          Description is too short min is 20</small>}
       </Form.Group> 
-
-      <ReactQuill value={content} onChange={setContent} placeholder='Leave a comment here' />
+      <Form.Group className='mb-3 w-75'>
+        <Form.Label>Main content</Form.Label>
+        <ReactQuill 
+          value={content} 
+          onChange={setContent} 
+          placeholder='Leave a comment here' 
+        />
+        {contentError && <small className="d-block form-text text-danger mt-2">
+          Content can't be empty</small>}
+      </Form.Group>
 
       <Button variant="primary" onClick={handleSubmit}>
        {actionText}
